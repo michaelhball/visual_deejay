535749e019b61522d3c474431e681a030ac0fe2d
michael <michael_ball@brown.edu>
2020-06-07 23:17:55 +0200

    minor stuff

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import cv2
 
-from visual_deejay.feature_extraction import extract_features_from_frame
+from visual_deejay.feature_extraction import extract_features_from_frame, extract_features_from_video
 
 
 if __name__ == '__main__':
-    file_path = "/Users/michaelball/Desktop/projects/visual_deejay/res/example_rekordbox_screenshot.png"
-    frame = cv2.imread(file_path)
-    extract_features_from_frame(frame)
+    # file_path = "/Users/michaelball/Desktop/projects/visual_deejay/res/example_rekordbox_screenshot.png"
+    # frame = cv2.imread(file_path)
+    # extract_features_from_frame(frame)
+
+    extract_features_from_video()
