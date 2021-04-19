5adb50b990e350aebbc46df2c576b06211d3d4c4
michaelhball <michael.h.s.ball@gmail.com>
2021-04-19 18:30:56 +0200

    ^

--- visual_deejay/stylegan_helpers.py
+++ visual_deejay/stylegan_helpers.py
@@ -34,22 +34,6 @@ def load_networks(file_path):
     return G, D, Gs, noise_vars
 
 
-def get_noise_range(out_size, generator_resolution):
-    """ Gets the correct noise dimensions for a given resolution of StyleGAN2
-
-    :param out_size: (int) output image size we want (can be different to generator e.g. doubling trick)
-    :param generator_resolution: (int) output image resolution of the generator we are using
-    :return: A range (min, max, & exponent) used to generate the sizes of the required noise vars.
-    """
-
-    log_max_res = int(np.log2(out_size))
-    log_min_res = 2 + (log_max_res - int(np.log2(generator_resolution)))
-    range_min = 2 * log_min_res + 1
-    range_max = 2 * (log_max_res + 1)
-    side_fn = lambda x: int(x / 2)
-    return range_min, range_max, side_fn
-
-
 def convert_z_to_w(Gs, latent, truncation_psi=0.7, truncation_cutoff=9):
     """ Converts from
 
